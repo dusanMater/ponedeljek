256236bd5347087966d3af239090b7892d4b7a17
dusanMater <dusan.fugina@sers.si>
2022-06-27 13:59:53 +0200

    ferf

--- shrani.php
+++ shrani.php
@@ -1,3 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+
+<h1>DElo s PB</h1>
 <?php
    $servername = "localhost";
    $username = "root";
@@ -22,7 +33,7 @@
    
    mysqli_close($conn);
 
-
-
-
 ?>
+</body>
+</html>
+
